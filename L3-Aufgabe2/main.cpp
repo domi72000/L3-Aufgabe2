57dd0333a4239af3f27c824c3f3282a0ebf36b84
Dominik Gogic <dominik.gogic@web.de>
2021-01-19 18:05:03 +0100

    Beispiel vom Breuer

---
+++ L3-Aufgabe2/main.cpp
@@ -0,0 +1,52 @@
+﻿//
+//	Inhalt:
+//		Projekt: 			SimSTB
+//		Thema:				Simulation digitaler und analoger Ein- und Ausgaben
+//		Datei:				Beispieldatei f�r die Nutzung der Schnittstelle
+//
+//	Autor:
+//		Name:				Markus Breuer
+//		Organisaion:		STB
+//
+//	Datum:
+//		Erstellt:			24.12.2018
+//		Letzte �nderung:	31.07.2019
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include "simulation.h"
+
+using namespace std;
+
+int main()
+{
+	bool	ende = false;
+	double	wert;
+
+	locale::global(locale("German_germany"));						// Zeichensatz deutsch
+
+	cout << "Beispielprogramm für Simulationsumgebung" << endl;
+	cout << "----------------------------------------" << endl << endl;
+	cout << "Die am analogen Eingang Kanal 0 liegenden Eingangssignale" << endl;
+	cout << "werden im Sekundentakt eingelesen. Beendet wird das Einlesen," << endl;
+	cout << "sobald am digitalen Eingang Kanal 0 eine 1 anliegt." << endl;
+	cout << "Am Ende werden noch ein digitales und analoges Ausgabesignal gesetzt." << endl << endl;
+
+	while (ende != true)
+	{
+		wert = anaEin(0);											// Einlesen eines analogen Eingabesignals über Kanal 0
+		cout << wert << endl;
+		Sleep(1000);
+		ende = digEin(0);											// Einlesen eines digitalen Eingabesignals über Kanal 0
+	}
+
+	digAus(15, 1);													// Ausgabe eines digitalen Ausgabesignals über Kanal 15
+	anaAus(7, -123.456);											// Ausgabe eines analogen Ausgabesignals über Kanal 7
+
+	cout << "Beispielprogramm beendet" << endl << endl;
+	system("Pause");
+	return 0;
+}
